c4ec77529629cb2c4b54f596e6ff230ab514cfd5
Chanoon Na <chanoonna@gmail.com>
2021-02-16 09:14:20 -0800

    src: Appointment component
    Basic layout and functionality is implemented.
    Confirm.js: Confirm message
    Empty.js: Shows empty schedule
    Error.js: Error on saving or deleting
    Form.js: Create or edit the schedule
    Header.js: Shows time
    Status.js: Loading screen on save or delete
    Show.js: Scheduled interview
    index.js: Entry point

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Show from 'components/Appointment/Show';
+import Header from 'components/Appointment/Header';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
+
+import 'components/Appointment/styles.scss';
+
+export default function Appointment() {
+
+  return (
+    <article className="appoinment">
+    </article>
+  );
+};
