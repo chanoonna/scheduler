eb7d6de0b83d3777602b41a732a6c9506713e226
Chanoon Na <chanoonna@gmail.com>
2021-02-21 20:19:03 -0800

    src: removed unnecessary spread operators

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -23,9 +23,9 @@ export default function useApplicationData() {
         dispatch(
           {
             type: 'SET_APPLICATION_DATA',
-            days: [...all[0].data],
-            appointments: {...all[1].data},
-            interviewers: {...all[2].data},
+            days: all[0].data,
+            appointments: all[1].data,
+            interviewers: all[2].data,
           }
         );
       });
