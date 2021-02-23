956c99dc9a6d4ffeedea0da3c950f9b85d9ab50b
Chanoon Na <chanoonna@gmail.com>
2021-02-22 17:16:32 -0800

    WebSocket is disabled for test purpose

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,13 @@
 import { useReducer, useEffect } from 'react';
 import axios from 'axios';
-import reducer from 'reducer/reducer';
+import reducer from "reducers/application";
+import constants from 'helpers/constants';
+
+const {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+} = constants;
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(
@@ -22,43 +29,44 @@ export default function useApplicationData() {
       .then(all => {
         dispatch(
           {
-            type: 'SET_APPLICATION_DATA',
-            days: all[0].data,
-            appointments: all[1].data,
-            interviewers: all[2].data,
+            type: SET_APPLICATION_DATA,
+            days: [...all[0].data],
+            appointments: {...all[1].data},
+            interviewers: {...all[2].data},
           }
         );
       });
     
-    const wss = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    // const wss = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
 
-    wss.onopen = function() {
-      console.log("Web Socket opened");
-      wss.send("ping");
-    };
+    // wss.onopen = function() {
+    //   console.log("Web Socket opened");
+    //   wss.send("ping");
+    // };
 
-    wss.onmessage = function(res) {
-      const appointment = JSON.parse(res.data);
+    // wss.onmessage = function(res) {
+    //   const appointment = JSON.parse(res.data);
 
-      if (appointment.type === 'SET_INTERVIEW') {
-        dispatch({ type: 'SET_INTERVIEW', id: appointment.id, interview: appointment.interview });
-      }
+    //   if (appointment.type === 'SET_INTERVIEW') {
+    //     dispatch({ type: 'SET_INTERVIEW', id: appointment.id, interview: appointment.interview });
+    //   }
+    // }
 
-      console.log(res.data);
-    }
-    return (() => wss.close());
+    // return (() => wss.close());
   }, []);
 
   function bookInterview(id, interview) {
-    return axios.put(`/api/appointments/${id}`, {...state.appointments[id], interview})
+    return axios.put(`/api/appointments/${id}`, { interview })
+      .then(() => dispatch({ type: SET_INTERVIEW, id, interview }));
   }
 
   function cancelInterview(id) {
     return axios.delete(`/api/appointments/${id}`)
+      .then(() => dispatch({ type: SET_INTERVIEW, id, interview: null }));
   }
 
   function setDay(day) {
-    dispatch({ type: 'SET_DAY', day });
+    dispatch({ type: SET_DAY, day });
   }
 
   return {
