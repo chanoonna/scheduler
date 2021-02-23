c900ab7df446dee83789c83189983b76223f9940
Chanoon Na <chanoonna@gmail.com>
2021-02-22 17:14:39 -0800

    test ids are given

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -25,9 +25,13 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={itemClass} onClick={() => props.setDay(props.name)}>
+    <li
+      data-testid="day"
+      className={itemClass}
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      <h3 data-testid="spots" className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 };
